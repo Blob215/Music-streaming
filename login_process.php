d7e972be0d7af1cf24b5f154059af8131ac45454
Blob215 <ryabov.b@oundleschool.org.uk>
2023-05-26 12:54:06 +0100

    e

--- login_process.php
+++ login_process.php
@@ -1,42 +1,23 @@
 <?php
 session_start();
 include_once ("connection.php");
-array_map("htmlspecialchars", $_POST);
-print_r($_POST);
-$stmt = $conn->prepare("SELECT * FROM tblusers WHERE username =:username ;" );
+
+$stmt = $conn->prepare("SELECT * FROM TblUsers WHERE Username =:username;");
 $stmt->bindParam(':username', $_POST['Username']);
+
 $stmt->execute();
-if ($stmt->rowCount() > 0) {
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-{ 
-    echo("sfdg");
-    $hashed= $row['Password'];
-    $attempt= $_POST['Pword'];
-    if(password_verify($attempt,$hashed)){
-        echo("Successful");
-        $_SESSION['loggedinID']=$row["UserID"];
-        if (!isset($_SESSION['backURL'])){
-            $backURL= "menu.php";
-        }
-        else{
-            $backURL=$_SESSION['backURL'];
-        }
-    }else{
-        header('Location: login.php');
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+    if ($_POST['password']==$row['Password']){
+        $_SESSION['name']=$row["Username"];
+        echo("<script> document.location.href='http://localhost/Library%20System/menu.php';
+        </script>");
     }
+    else{
+        echo("<script> document.location.href='http://localhost/Library%20System/Login.php';
+        </script>");
 }
-  } else {
-    header('Location: login.php');
-  }
-
+}
 $conn=null;
-?>
-
-<?php
-session_start(); 
-if (!isset($_SESSION['loggedinID']))
-{   
-    $_SESSION['backURL'] = $_SERVER['REQUEST_URI'];
-    header("Location:login.php");
-}
+;
 ?>
