43ca4a005aabb68cf88dac83fdeade4a7bbb22ed
Blob215 <ryabov.b@oundleschool.org.uk>
2023-11-20 11:19:02 +0000

    Better styling and UI

--- Signup.php
+++ Signup.php
@@ -5,15 +5,26 @@
     <!-- Link to stylesheets and bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="stylesheet.css">
-    <style>
-        body {
-            background-color:#000000
-        }
+
+<style>
+    .container-fluid{ background-color: #000000;
+        top: 0;
+    width: 100%}
+        .navbar navbar-inverse  {background-color: #000000;}
+        .navbar-brand a {color:black;}
+        .navbar-header a{color:black;}
     </style>
 </head>
 <body>
-    <h1 class="signup">Sign Up</h1>
-<form class="signup" action='addusers.php' method="POST">
+<nav class="navbar navbar-inverse" style="background-color:#000000; class='font-family: 'Times New Roman', Times, serif;">
+    <div class = "container-fluid">
+        <ul class="nav navbar-nav">
+            <li><h1 class="login">Sign Up</h1></li>
+        </ul>
+    </div>
+    </nav>
+    <br>
+<form class="About" action='addusers.php' method="POST">
     UserID:<input type="text" name="userid"><br>
     Forename:<input type="text" name="forename"><br>
     Surname:<input type="text" name="surname"><br>
@@ -22,6 +33,10 @@
     Emailaddress:<input type="text" name="emailaddress"><br>
     Phone Number:<input type="text" name="phonenumber"><br>
     <input type="submit" value="Sign Up"><br>
+
+    <div style="position: absolute; bottom: 5px; background-color: black">
+    <li><a href="menu.php">Go back to homepage</a></li>
+    </div>
 </form>
 </body>
 </html>
--- login.php
+++ login.php
@@ -3,18 +3,34 @@
 <head>
    <title>Login</title>
    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
    <style>
-        body {
-            background-color:#000000
-        }
+    .container-fluid{ background-color: #000000;
+        top: 0;
+    width: 100%}
+        .navbar navbar-inverse  {background-color: #000000;}
+        .navbar-brand a {color:black;}
+        .navbar-header a{color:black;}
     </style>
 </head>
 <body>
-
-<form action="login_process.php" method= "POST"  class="login">
+<nav class="navbar navbar-inverse" style="background-color:#000000; class='font-family: 'Times New Roman', Times, serif;">
+    <div class = "container-fluid">
+        <ul class="nav navbar-nav">
+            <li><h1 class="login">Login</h1></li>
+        </ul>
+    </div>
+    </nav>
+    <br>
+<form action="login_process.php" method= "POST"  class="About">
  User name:<input type="text" name="Username"><br>
  Password:<input type="password" name="Password"><br>
   <input type="submit" value="Login">
+
+  <div style="position: absolute; bottom: 5px; background-color: black">
+    <li><a href="menu.php">Go back to homepage</a></li>
+    </div>
 </form>
 </body>
 </html>
