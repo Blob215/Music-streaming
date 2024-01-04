5685ca97d71aca683bf355d1e1397542fbfd0b82
Blob215 <ryabov.b@oundleschool.org.uk>
2024-01-04 18:09:29 +0400

    Able to view library

--- Library.php
+++ Library.php
@@ -17,16 +17,23 @@ if (!isset($_SESSION['UserID']))
     include_once('connection.php');
 ?>
 <?php
-    $stmt=$conn->prepare("SELECT MusicID FROM tblLibrary WHERE UserID = :userID");
+    $stmt=$conn->prepare("SELECT  tblmusic.artist as art, tblmusic.image as img, tblmusic.SongTitle as st FROM tblLibrary
+    INNER JOIN tblmusic on tblmusic.MusicID = tbllibrary.MusicID
+    WHERE tbllibrary.UserID = :userID");
     $stmt->bindParam(':userID', $_SESSION['UserID'], PDO::PARAM_INT);
     $stmt->execute();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
 {
-    echo ($row["Artist"]);
+    echo ($row["art"]);
+
     echo "<br />";
-    echo ($row["SongTitle"]);
+
+    echo ($row["st"]);
+
     echo "<br />";
-    echo ("<img width='200' length='200' src=images/".$row["Image"].">");
+
+    echo ("<img width='200' length='200' src=images/".$row["img"].">");
+
     echo "<br />";
 }
 ?>
