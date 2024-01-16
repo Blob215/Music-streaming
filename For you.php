965ec2283c72ef4e484385e5cfcd59c9b6f981b6
Blob215 <ryabov.b@oundleschool.org.uk>
2024-01-16 19:44:39 +0000

    Added for you page

--- For you.php
+++ For you.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['UserID']))
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Library</title>
+    <title>For you</title>
 </head>
 <body>
 <?php
@@ -17,16 +17,18 @@ if (!isset($_SESSION['UserID']))
     include_once('connection.php');
 ?>
 <?php
-    $stmt=$conn->prepare("SELECT  tblmusic.artist as art, tblmusic.image as img, tblmusic.SongTitle as st FROM tblLibrary
-    INNER JOIN tblmusic on tblmusic.MusicID = tbllibrary.MusicID
-    WHERE tbllibrary.UserID = :userID");
+    $stmt=$conn->prepare("SELECT  tblmusic.artist as art, tblmusic.image as img, tblmusic.SongTitle as st FROM tblPreferences
+    INNER JOIN tblmusic on tblmusic.Genre = tblPreferences.Genre
+    WHERE tblPreferences.UserID = :userID");
     $stmt->bindParam(':userID', $_SESSION['UserID'], PDO::PARAM_INT);
     $stmt->execute();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
-{
-    echo'<form action="addtolibrary.php" method="post">';
-    echo "<br />";
+    {
     
+    echo'<form action="addtolibrary.php" method="post">';
+
+    echo "<br />";
+
     echo ("<img width='200' length='200' src=images/".$row["img"].">");
 
     echo "<br />";
@@ -38,8 +40,9 @@ if (!isset($_SESSION['UserID']))
     echo ($row["st"]);
 
     echo "<br />";
-
-
+    
+    "<input type='submit' value='Add to library'><input type='hidden' name='MusicID'
+    value=".$row['MusicID']."><br></form>";
 }
 ?>
 
