2e553d8e92df588fa66d1a2dc7e68d7efb91af49
Blob215 <ryabov.b@oundleschool.org.uk>
2024-03-18 14:04:44 +0000

    Fixed issue with admin being able to remove other admins

--- changeusers.php
+++ changeusers.php
@@ -27,16 +27,18 @@ if ($users == 1) {
         echo ('<div class="User-ID">');
             if ($userData['Role'] == 1) {
                 echo ('<h3>Admin</h3>');
+                echo ($userData["Username"]);
             }
             else {
                 echo ('<h3>User</h3>');
+                echo ('</div>');
+                echo ($userData["Username"]);
+                echo ('<div class="remove">');
+                echo ('<a class="link" href="removeusers.php?UserID=' . $userData["UserID"] .'">X</a>');
+                echo ('</div>');
+                echo('</div>');
             }
-        echo ('</div>');
-        echo ($userData["Username"]);
-        echo ('<div class="remove">');
-            echo ('<a class="link" href="removeusers.php?UserID=' . $userData["UserID"] .'">X</a>');
-        echo ('</div>');
-    echo('</div>');
+        
     } while ($userData = $stmt->fetch(PDO::FETCH_ASSOC));
 } else {
     // error message if there are no users
