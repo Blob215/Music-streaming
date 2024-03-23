01566d7b9da79cb2a3d3e1f00b93c3b1c6031c50
Blob215 <ryabov.b@oundleschool.org.uk>
2024-03-23 11:17:04 +0000

    Changed name of files

--- removeusers.php
+++ removeusers.php
@@ -1,13 +1,51 @@
+<!DOCTYPE html>
+<html>
+<title>Delete Users</title>
 <?php
+
 include_once("connection.php");
-session_start(); 
-array_map("htmlspecialchars", $_GET);
 
-// removes the user from the database
-$stmt = $conn->prepare("DELETE FROM tblusers WHERE UserID = :userID");
-$stmt->bindParam(':userID', $_GET['UserID'], PDO::PARAM_INT);
+session_start();
+
+if (!isset($_SESSION['UserID']))
+{   
+    $_SESSION['backURL'] = $_SERVER['REQUEST_URI'];
+    header("Location:login.php");
+}
+
+// fetch all users
+$stmt = $conn->prepare("SELECT * FROM tblUsers ORDER BY username");
 $stmt->execute();
+$userData = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// sends the user to the change users page
-header('Location: changeusers.php');
+if ($userData) {
+    $users = 1;
+}
+else {
+    $users = 0;
+}
+if ($users == 1) {
+    do {
+        echo ('</div>');
+        echo ('<div class="User-ID">');
+            if ($userData['Role'] == 1) {
+                echo ('<h3>Admin</h3>');
+                echo ($userData["Username"]);
+            }
+            else {
+                echo ('<h3>User</h3>');
+                echo ('</div>');
+                echo ($userData["Username"]);
+                echo ('<div class="remove">');
+                echo ('<a class="link" href="removeusers.php?UserID=' . $userData["UserID"] .'">X</a>');
+                echo ('</div>');
+                echo('</div>');
+            }
+        
+    } while ($userData = $stmt->fetch(PDO::FETCH_ASSOC));
+} else {
+    // error message if there are no users
+    echo('<br>No users to display');
+}
 ?>
+</html>
